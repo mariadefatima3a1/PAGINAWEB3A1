b7b5ad52cdc1073b299fb8691b11f8503d5f2900
Maria de Fatima de Jesus Souza <00001106914831SP@al.educacao.sp.gov.br>
2024-11-27 18:05:29 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,53 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Tema CSS',
+    'O que é CSS?',
+    'É uma linguagem para estilizar páginas HTML'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Tema CSS',
+    'Para que serve o id no CSS?',
+    'Estilizar um elemento único'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Tema CSS',
+    ' Como mudar a cor de fundo com CSS?',
+    'Use background-color'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Tema HTML',
+    'Para que serve a tag <a>?',
+    'Criar links'
 )
 
 criaCartao(
-    'aaaaaaaa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Tema HTML',
+    ' O que é uma tag <div>?',
+    'Um contêiner de bloco para agrupar elementos'
 )
 
 criaCartao(
-    'bbbbbbbb',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Tema HTML',
+    'Como criar uma lista numerada?',
+    'Use a tag <ol> com <li>'
 )
 
 criaCartao(
-    'Lingua cccccc',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Tema JavaScript',
+    ' O que é JavaScript?',
+    'Linguagem para adicionar interatividade em páginas web'
 )
 
 criaCartao(
-    'dddddddddd inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Tema JavaScript',
+    ' Como exibir uma mensagem de alerta?',
+    'Use alert()'
 )
 
 criaCartao(
-    'Lingua eeeeeeeeee',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Tema JavaScript',
+    'Para que serve o let no JavaScript?',
+    'Declarar variáveis com escopo de bloco'
 )
